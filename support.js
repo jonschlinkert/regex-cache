2d679a35d00942fc126f7d5f5769ce5be303cb47
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-17 05:50:42 -0500

    adds tests

--- support.js
+++ support.js
@@ -11,9 +11,29 @@ exports.toRegex = function toRegex(str, opts) {
   if (opts.a) re += 'a';
   if (opts.b) re += 'b';
   if (opts.c) re += 'c';
+  if (opts.d && opts.d.e) re += opts.d.e;
+
+  if (opts.e) re += 'e';
+  if (opts.f) re += 'f';
+  if (opts.g) re += 'g';
+  if (opts.h) re += 'h';
+  if (opts.i) re += 'i';
+  if (opts.j) re += 'j';
+  if (opts.k) re += 'k';
+  if (opts.l) re += 'l';
+  if (opts.m) re += 'm';
+  if (opts.n) re += 'n';
+  if (opts.o) re += 'o';
+  if (opts.p) re += 'p';
+  if (opts.q) re += 'q';
+  if (opts.r) re += 'r';
+  if (opts.s) re += 's';
+  if (opts.t) re += 't';
+
   if (opts.x) re += 'x';
   if (opts.y) re += 'y';
   if (opts.z) re += 'z';
+
 
   var f = opts.flags || '';
   re = re
@@ -30,5 +50,6 @@ exports.toRegex = function toRegex(str, opts) {
   if (re.indexOf('z') !== -1) {
     re = re.replace(/z/g, '');
   }
+
   return new RegExp(re, f);
 };
--- test.js
+++ test.js
@@ -7,7 +7,7 @@
 
 'use strict';
 
-var util = require('util');
+var assert = require('assert');
 var should = require('should');
 var cache = require('./');
 var fn = require('./support');
@@ -22,6 +22,12 @@ describe('regex-cache:', function () {
     });
   });
 
+  describe('when a regex is passed:', function () {
+    it('should return it directly:', function () {
+      cache(fn.toRegex()).should.eql(/([^W]?)/);
+    });
+  });
+
   describe('when a function is passed:', function () {
     it('should call the function when no other arguments are passed:', function () {
       str.match(cache(fn.toRegex))[1].should.equal('a');
@@ -29,7 +35,7 @@ describe('regex-cache:', function () {
 
     it('should cache the results on the `_default_` key:', function () {
       str.match(cache(fn.toRegex))[1].should.equal('a');
-      cache.cache.should.have.property('_default_');
+      cache.basic.should.have.property('_default_');
     });
   });
 
@@ -42,45 +48,55 @@ describe('regex-cache:', function () {
       var d = str.match(cached);
       var e = str.match(cached);
       e[1].should.equal('a');
-      cache.cache.should.have.property('_default_');
+      cache.basic.should.have.property('_default_');
     });
 
     it('should work when a regex is passed:', function () {
       str.match(cache(fn.toRegex()))[1].should.equal('a');
-      cache.cache.should.have.property('_default_');
+      cache.basic.should.have.property('_default_');
     });
 
     it('should work when a function is passed:', function () {
       str.match(cache(fn.toRegex))[1].should.equal('a');
-      cache.cache.should.have.property('_default_');
+      cache.basic.should.have.property('_default_');
     });
 
     it('should work when options are passed:', function () {
       str.match(cache(fn.toRegex()))[1].should.equal('a');
       str.match(cache(fn.toRegex, 'foo'))[1].should.equal('foo');
+      assert.equal(str.match(cache(fn.toRegex, 'bar')), null);
       str.match(cache(fn.toRegex, 'foo', { a: true, b: true, c: true }))[1].should.equal('fooabc');
-      str.match(cache(fn.toRegex, { a: true, b: true, c: true }))[1].should.equal('abc');
       str.match(cache(fn.toRegex, { a: true, b: true, c: true, flags: 'g'}))[0].should.equal('abc');
+    });
+
+    it('should work when an option is an object:', function () {
+      str.match(cache(fn.toRegex, 'foo', { d: {e: 'abc'} }))[0].should.equal('fooabc');
+      str.match(cache(fn.toRegex, 'foo', { d: {e: 'ab'} }))[0].should.equal('fooab');
+      str.match(cache(fn.toRegex, 'foo', { d: {f: 'abc'} }))[0].should.equal('foo');
+    });
+
+    it('should not use a cached regex when options have changed:', function () {
+      str.match(cache(fn.toRegex, { a: true, b: true, c: true }))[1].should.equal('abc');
+      // change `a` to false
+      str.match(cache(fn.toRegex, { a: false, b: true, c: true }))[1].should.equal('bc');
+      str.match(cache(fn.toRegex, { a: false, b: true, c: true }))[1].should.not.equal('abc');
     });
   });
 
   describe('cache object:', function () {
     it('should expose the cache:', function () {
-      cache(fn.toRegex());
-      cache.cache.should.have.property('_default_');
-
       cache(fn.toRegex, 'foo');
-      cache.cache.should.have.property('foo');
+      cache.basic.should.have.property('foo');
 
-      cache(fn.toRegex, 'foo', { a: true, b: true, c: true });
-      cache.cache.should.have.property('fooatruebtruectrue');
+      cache(fn.toRegex, 'bar', { a: true, b: true, c: true });
+      cache.cache.should.have.property('bar');
 
-      cache(fn.toRegex, { a: true, b: true, c: true });
-      cache.cache.should.have.property('atruebtruectrue');
+      // cache(fn.toRegex, { a: true, b: true, c: true });
+      // // cache.cache.should.have.property('atruebtruectrue');
 
-      cache(fn.toRegex, { a: true, b: true, c: true, flags: 'g'});
-      cache.cache.should.have.property('atruebtruectrueflagsg');
-      cache.cache.should.have.property('foo');
+      // cache(fn.toRegex, { a: true, b: true, c: true, flags: 'g'});
+      // cache.cache.should.have.property('atruebtruectrueflagsg');
+      // cache.cache.should.have.property('foo');
     });
   });
 });
